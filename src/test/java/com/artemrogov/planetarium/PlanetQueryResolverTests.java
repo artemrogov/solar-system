f3863058d375327f815c345fe68b99fdeb68513c
Рогов Артем Владимирович <rogar@gkomega.ru>
2023-11-05 18:16:19 +0300

    bug fix

--- src/test/java/com/artemrogov/planetarium/PlanetQueryResolverTests.java
+++ src/test/java/com/artemrogov/planetarium/PlanetQueryResolverTests.java
@@ -1,7 +1,6 @@
 package com.artemrogov.planetarium;
 
 import com.artemrogov.planetarium.resolvers.query.PlanetQueryResolver;
-import com.artemrogov.planetarium.service.PlanetServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
