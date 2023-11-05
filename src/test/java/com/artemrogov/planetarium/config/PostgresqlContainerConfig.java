fbd724100d21d5cea60bc8d4dc3915b153426c61
Рогов Артем Владимирович <rogar@gkomega.ru>
2023-11-05 18:29:43 +0300

    connected dependencies for tests and configured a test container for the database

---
+++ src/test/java/com/artemrogov/planetarium/config/PostgresqlContainerConfig.java
@@ -0,0 +1,34 @@
+package com.artemrogov.planetarium.config;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PostgresqlContainerConfig {
+    private static final String image = "postgresql:latest";
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(image);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+}
